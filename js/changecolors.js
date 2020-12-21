3129750a54866761d26e4bf5aa9f9ea619357d6e
nicholasklaene <nicholasklaene@outlook.com>
2020-12-21 11:42:41 -0500

    Changed colors slightly to make them easier on the eyes, took save message out of normal document flow

--- js/changecolors.js
+++ js/changecolors.js
@@ -18,23 +18,23 @@ let changeColors = function (currentMode = defaultMode) {
     let wrapper = document.getElementById('wrapper');
     switch (currentMode) {
         case "light":
-            wrapper.style.backgroundColor = "#fff"
+            wrapper.style.backgroundColor = "#eee"
             lightButton.style.border = "2px solid var(--primary-color)";
-            darkButton.style.border = "2px solid #000";
-            darkButton.style.backgroundColor = "#000";
+            darkButton.style.border = "2px solid #111";
+            darkButton.style.backgroundColor = "#111";
             for (let button of buttons) {
                 button.style.backgroundColor = "var(--primary-color)";
-                button.style.color = "#fff";
+                button.style.color = "#eee";
             }
             defaultMode = "light";
             break;
         case "dark":
-            wrapper.style.backgroundColor = "#000"
-            lightButton.style.border = "2px solid #fff";
+            wrapper.style.backgroundColor = "#111"
+            lightButton.style.border = "2px solid #eee";
             darkButton.style.border = "2px solid var(--primary-color)";
-            darkButton.style.backgroundColor = "#000";
+            darkButton.style.backgroundColor = "#111";
             for (let button of buttons) {
-                button.style.backgroundColor = "#000";
+                button.style.backgroundColor = "#111";
                 button.style.border = "1px solid var(--primary-color)"
                 button.style.color = "var(--primary-color)";
             }
