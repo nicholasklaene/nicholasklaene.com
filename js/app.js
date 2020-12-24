88fd618e651984d0caef64d1b90fbae56fa18c9c
nicholasklaene <nicholasklaene@outlook.com>
2020-12-24 16:04:22 -0500

    fixed everything except CORS error

--- js/app.js
+++ js/app.js
@@ -33,7 +33,6 @@ if (location.href.split("/").slice(-1) == "about.html") {
 `
 };
 
-
 htmlToElement = html => {
   let template = document.createElement('template');
   html = html.trim();
@@ -45,40 +44,3 @@ const navHTML = htmlToElement(navTemplate);
 const footerHTML = htmlToElement(footerTemplate);
 body.prepend(navHTML);
 body.append(footerHTML);
-
-// API
-(() => {
-  const form = document.querySelector('form');
-  const submitResponse = document.querySelector('#response');
-  const formURL = 'https://uw9atfv9zl.execute-api.us-east-1.amazonaws.com/Prod/submitForm';
-
-  form.onsubmit = e => {
-    e.preventDefault();
-
-    // Capture the form data
-    let data = {};
-    Array.from(form).map(input => (data[input.id] = input.value));
-    delete data.submit;
-    console.log('Sending: ', JSON.stringify(data));
-    submitResponse.innerHTML = 'Sending...';
-
-    // Create the AJAX request
-    var xhr = new XMLHttpRequest();
-    xhr.open(form.method, formURL, true);
-    xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-    // Send the collected data as JSON
-    xhr.send(JSON.stringify(data));
-
-    xhr.onloadend = response => {
-      if (response.target.status === 200) {
-        form.reset();
-        submitResponse.innerHTML = 'Message sent!';
-      } else {
-        submitResponse.innerHTML = 'Error! Please try again.';
-        console.error(JSON.parse(response));
-      }
-    };
-  };
-})();
