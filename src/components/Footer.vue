08d0feb54d6e7880a509c8067b3418bd4ed050de
unknown <nicholasklaene@outlook.com>
2021-07-31 11:59:41 -0400

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <a href="https://github.com/nicholasklaene" target="_blank">Github</a>
+    <a href="https://www.linkedin.com/in/nicholas-klaene/" target="_blank">
+      Linkedin
+    </a>
+    <a href="resume.pdf" target="_blank">Resume</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  width: 100%;
+  display: flex;
+  padding: 4rem 0;
+  gap: 2rem;
+  justify-content: center;
+  background-color: #e5e7eb7f;
+  a {
+    text-decoration: none;
+    color: #487eb0;
+    font-size: 0.9rem;
+  }
+}
+</style>
