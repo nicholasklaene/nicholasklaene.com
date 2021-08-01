e249925f372bc34bea1fb4ae63631b31b495f48a
unknown <nicholasklaene@outlook.com>
2021-07-31 23:55:28 -0400

    resume

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
     <a href="https://www.linkedin.com/in/nicholas-klaene/" target="_blank">
       Linkedin
     </a>
-    <a href="resume.pdf" target="_blank">Resume</a>
+    <a href="@/assets/resume.pdf" target="_blank">Resume</a>
   </footer>
 </template>
 
